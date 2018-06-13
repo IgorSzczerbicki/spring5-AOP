437e6e42bfc71b3415f53d9fc108770553b64b85
Igor Szczerbicki <pl4cek@outlook.com>
2018-06-13 09:25:21 +0200

    Okreslanie kolejnosci wykonywania Aspect'ow

--- src/com/nauka/spring5/aop/aspect/PointcutAspect.java
+++ src/com/nauka/spring5/aop/aspect/PointcutAspect.java
@@ -3,10 +3,12 @@ package com.nauka.spring5.aop.aspect;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(1)
 public class PointcutAspect {
 
 	//zdefiniowanie wyrazenia Pointcut
