d92fe8f3c54f192602e6483320a04877b439e524
Igor Szczerbicki <pl4cek@outlook.com>
2018-06-14 09:02:38 +0200

    Przechwytywanie rzucanych wyjatkow

--- src/com/nauka/spring5/aop/dao/ClientDAO.java
+++ src/com/nauka/spring5/aop/dao/ClientDAO.java
@@ -8,9 +8,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class ClientDAO {
 
-	public List<String> getClient(String name) {
+	public List<String> getClient(String n) throws Exception {
+		String name = testName(n);
 		List<String> client = new ArrayList<>();
 		client.add(name);
 		return client;
+	}
+	
+	public String testName(String name) throws Exception {
+		if ("rzuc".equals(name)) {
+			throw new RuntimeException();
+		} else return name;
 	}
 }
