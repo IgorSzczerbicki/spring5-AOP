fe545c95a885404b95904699b9bdd32ff4037cbc
Igor Szczerbicki <pl4cek@outlook.com>
2018-06-15 17:01:57 +0200

    Dodanie loggera  java.util.logging.Logger

--- src/com/nauka/spring5/aop/aspect/AroundAspect.java
+++ src/com/nauka/spring5/aop/aspect/AroundAspect.java
@@ -1,5 +1,7 @@
 package com.nauka.spring5.aop.aspect;
 
+import java.util.logging.Logger;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -8,6 +10,8 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect
 public class AroundAspect {
+	
+	private static Logger log = Logger.getLogger(AroundAspect.class.getName());
 	
 	@Around("execution(String com.nauka.spring5.aop.dao.CardDAO.getCard(..))")
 	public Object getCardExecutionTime(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
@@ -20,7 +24,7 @@ public class AroundAspect {
 		long end = System.currentTimeMillis();
 		
 		long dur = end - begin;
-		System.out.println("$$$$$ @Around duration: " + dur);
+		log.info("$$$$$ @Around duration: " + dur);
 		
 		return result;
 	}
