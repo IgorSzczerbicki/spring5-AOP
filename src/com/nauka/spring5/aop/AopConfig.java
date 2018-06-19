b23927ce962d391eb61ee4acee799439aad31ecb
Igor Szczerbicki <pl4cek@outlook.com>
2018-06-19 09:23:14 +0200

    Konfiguracja w JAVA

--- src/com/nauka/spring5/aop/AopConfig.java
+++ src/com/nauka/spring5/aop/AopConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-//@Configuration
-//@EnableAspectJAutoProxy
-//@ComponentScan("com.nauka.spring5.aop")
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.nauka.spring5.aop")
 public class AopConfig {}
--- src/com/nauka/spring5/mvc/AopConfig.java
+++ src/com/nauka/spring5/mvc/AopConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-//@Configuration
-//@EnableAspectJAutoProxy
-//@ComponentScan("com.nauka.spring5.mvc")
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.nauka.spring5.mvc")
 public class AopConfig {}
